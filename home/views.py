06c2cb7451f5ca675c826e9a7825f02ce787394f
DavidHuang <d65044713@gmail.com>
2020-10-19 22:11:55 -0700

    Datatable 處理

--- home/views.py
+++ home/views.py
@@ -49,6 +49,7 @@ def inputtojson(input):
     }
     return data
 
+
 def input(request):
     inputdata = request.GET
     #print('not json : ')
@@ -77,16 +78,27 @@ def index(request):
     return render(request, 'index.html')
 
 def table(request):
+    # data = {"data": [
+    #     {
+    #         "id": "10000001",
+    #         "cid": "20454554",
+    #         "city": "Corvallis",
+    #         "latitude": "1112.5555",
+    #         "longitude": "134.54684",
+    #         "street_name": "SW Chickadee ST",
+    #         "injured_count": "4",
+    #         "crash_time": "05/11/2015"
+    #     },
+    # ]
+    # }
     data = {"data": [
         {
-            "id": "10000001",
-            "cid": "20454554",
-            "city": "Corvallis",
-            "latitude": "1112.5555",
-            "longitude": "134.54684",
-            "street_name": "SW Chickadee ST",
-            "injured_count": "4",
-            "crash_time": "05/11/2015"
+            "UPS_Service": "David_test",
+            "Money": "56565",
+        },
+        {
+            "UPS_Service": "David_test2",
+            "Money": "1265",
         },
     ]
     }
