6755caf454ceef83b2362a90a020478e16a58988
MeCBing <j126j126@gmail.com>
2020-10-22 21:24:47 -0700

    change debug to false

--- cs561_shipping/settings.py
+++ cs561_shipping/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'n+bsa0sok=bdm2(f034%v!vxiy!ossa$cmfo2e$2spd-hv!vp^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
