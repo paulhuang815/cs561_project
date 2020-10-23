aa9a560d8345eb66de8ae647fb80081756c2dde4
happyNull <www111222com@qq.com>
2020-10-23 14:29:59 -0700

    The change of js and index.html

--- home/static/js/demo.js
+++ home/static/js/demo.js
@@ -1,157 +1,89 @@
 $(document).ready(function () {
-    // $('#SQLSearch').on('click', function () {
-    //         console.log($('#SQLSearchTextarea').val())
-    //         $('#datatable').DataTable().clear().draw();
-    //         $('#datatable').DataTable().columns.adjust().draw();
-    //         // $('#datatable').DataTable().draw()
-    //     }
-    // )
 
-    // $('#submit').click(function () {
-    //     // frm.submit(function () {
-    //     var csrfmiddlewaretoken = $("#add_member_Form").find("input[name='csrfmiddlewaretoken']" ).val();
-    //     var formData = $('#search_form').serializeArray();
-    //     formData = JSON.stringify(formData);
-    //     $.ajax({
-    //         url: '{% url "searchForm" %}',
-    //         data: {
-    //             "csrfmiddlewaretoken" : csrfmiddlewaretoken,
-    //             "formData" : formData
-    //         },
-    //         type: "POST",
-    //         dataType: "json",
-    //         success: function(result) {
-    //             alert(result.Result);
-    //         },
-    //         error: function(xhr, ajaxOptions, thrownError){
-    //             alert(thrownError + '\n' + xhr.status + '\n' + ajaxOptions);
-    //         }
-    //     });
+    // var table = $('#datatable').DataTable({
+    //     "processing": true,
+    //     "serverSide": true,
+    //     "deferRender": true,
+    //     "iDisplayLength": 25,
+    //     "paging": true,
+    //     "deferLoading": 0, //載入時不執行查詢
+    //     "ajax": {
+    //         url: '/input',
+    //         // url: '../views/table',
+    //         type: 'GET',
+    //         dataSrc: 'data',
+    //     },
+    //     "columns": [
+    //         {"data": "Company"},
+    //         {"data": "Service"},
+    //         {"data": "Money"}
+    //     ]
     // });
-    // $('#datatable').DataTable({
-    //         "processing": true,
-    //         "serverSide": true,
-    //         "deferRender": true,
-    //         "iDisplayLength": 25,
-    //         "paging": true,
-    //         "ajax": {
-    //             url: '{% url "input" %}',
-    //             // url: '../views/table',
-    //             type: 'GET',
-    //             dataSrc: 'data',
-    //         },
-    //         "columns": [
-    //             {"data": "UPS_Service"},
-    //             {"data": "Money"}
-    //         ]
-    // });
-
-    // $('#submit').click(function () {
-    //     var csrfmiddlewaretoken = $("#add_member_Form").find("input[name='csrfmiddlewaretoken']" ).val();
-    //     var formData = $('#search_form').serializeArray();
-    //$('#submit').click(function () {
-        // frm.submit(function () {
-    //    var csrfmiddlewaretoken = $("#add_member_Form").find("input[name='csrfmiddlewaretoken']" ).val();
-    //    var formData = $('#search_form').serializeArray();
-    //    formData = JSON.stringify(formData);
-    //    $.ajax({
-    //        url: '{% url "searchForm" %}',
-    //        data: {
-    //            "csrfmiddlewaretoken" : csrfmiddlewaretoken,
-    //            "formData" : formData
-    //        },
-    //        type: "POST",
-    //        dataType: "json",
-    //        success: function(result) {
-    //            alert(result.Result);
-    //        },
-    //        error: function(xhr, ajaxOptions, thrownError){
-    //            alert(thrownError + '\n' + xhr.status + '\n' + ajaxOptions);
-    //        }
-    //    });
-    //});
-
-    // $('#submit').click(function () {
-    //
-    //     $('#datatable').DataTable({
-    //         "processing": true,
-    //         "serverSide": true,
-    //         "deferRender": true,
-    //         "iDisplayLength": 25,
-    //         "paging": true,
-    //         "ajax": {
-    //             url: '{% url "input" %}',
-    //             // url: '../views/table',
-    //             type: 'GET',
-    //             data: 'data',
-    //             // data: {
-    //             //     "Dimension units": $('#Dimension_units').val(),
-    //             //     "Height": $('#Height').val(),
-    //             //     "Length": $('#Length').val(),
-    //             //     "Width": $('#Width').val(),
-    //             //     "Weight unit": $('#Weight_unit').val(),
-    //             //     "Weight": $('#Weight').val(),
-    //             //     "ShipFrom": {
-    //             //         "Address": {
-    //             //             "AddressLine": $('#From_AddressLine').val(),
-    //             //             "City": $('#From_City').val(),
-    //             //             "CountryCode": $('#From_CountryCode').val(),
-    //             //             "PostalCode": $('#From_PostalCode').val(),
-    //             //             "StateProvinceCode": $('#From_StateProvinceCode').val()
-    //             //         }
-    //             //     },
-    //             //     "ShipTo": {
-    //             //         "Address": {
-    //             //             "AddressLine": $('#To_AddressLine').val(),
-    //             //             "City": $('#To_City').val(),
-    //             //             "CountryCode": $('#To_CountryCode').val(),
-    //             //             "PostalCode": $('#To_PostalCode').val(),
-    //             //             "StateProvinceCode": $('#To_StateProvinceCode').val()
-    //             //         }
-    //             //     }
-    //             // },
-    //             dataType: 'json',
-    //             dataSrc: '',
-    //             success: function(result) {
-    //                 alert(result.Result);
-    //             },
-    //             error: function(xhr, ajaxOptions, thrownError){
-    //                 alert(thrownError + '\n' + xhr.status + '\n' + ajaxOptions);
-    //             }
-    //             // "data": function (d) {
-    //             //     d.query = $('#SQLSearchTextarea').val()
-    //             // }
-    //         },
-    //         "columns": [
-    //             {"data": "UPS_Service"},
-    //             {"data": "Money"}
-    //         ]
-    //     });
-    // });
-
-    var table = $('#datatable').DataTable({
-            "processing": true,
-            "serverSide": true,
-            "deferRender": true,
-            "iDisplayLength": 25,
-            "paging": true,
-            "deferLoading": 0, //載入時不執行查詢
-            "ajax": {
-                url: '/input',
-                // url: '../views/table',
-                type: 'GET',
-                dataSrc: 'data',
-            },
-            "columns": [
-                {"data": "Company"},
-                {"data": "Service"},
-                {"data": "Money"}
-            ]
-    });
 
     $('#submit').click(function () {
-          table.ajax.url('input/?' + $('#search_form').serialize()).load();
-          return false;
-    });
+        // table.ajax.url('input/?' + $('#search_form').serialize()).load();
+        // return false;
 
-});
+        $.ajax({
+            url: '{% url "input" %}',
+            type: 'GET',
+            tradition: true,
+            async : false,
+            data: $('#search_form').serialize(),
+            dataType: "json",
+            success: function (data) {
+                $('#datatable').DataTable({
+                            "processing": true,
+                            "serverSide": true,
+                            "deferRender": true,
+                            "iDisplayLength": 25,
+                            "paging": false,
+                            "deferLoading": 0,
+                            retrieve:true
+                            }
+                        )
+                tb = document.getElementById('datatable');
+                var rowNum=tb.rows.length;
+                for (i=1;i<rowNum;i++) {
+                    tb.deleteRow(i);
+                    rowNum=rowNum-1;
+                    i=i-1;
+                }
+                var tbody = document.createElement("tbody");
+                        for (var i in data.data){
+                            var tr = document.createElement("tr");
+                                for(var j in data.data[i]){
+                                if(j == 0){
+                                    if(data.data[i][j] == "UPS"){
+                                        var td = document.createElement("td");
+                                        td.innerHTML=data.data[i][j];
+                                       tr.appendChild(td);
+                                    }
+                                }
+                                else{
+                                    var td = document.createElement("td");
+                                    td.innerHTML=data.data[i][j];
+                                    tr.appendChild(td);
+                                }
+                            }
+                            var td2 = document.createElement("td");
+                            var butt=document.createElement("a");
+                            butt.href = 'https://www.ups.com/us/en/global.page';
+                            butt.type = 'button';
+                            butt.setAttribute('class', "btn btn-primary btn-sm");
+                            butt.innerHTML = "skip";
+                            td2.appendChild(butt);
+                            tr.appendChild(td2);
+                            tbody.appendChild(tr);
+                        }
+                        tb.appendChild(tbody);
+
+                //document.getElementById('table1').style.visibility="hidden";
+            },
+            error:function (){
+                alert('eqeqqq');
+            }
+        });
+        return false;
+    });
+})
